89ec699aca9bdadf1daee7840ee3cdea32a0c89a
David Craven <david@craven.ch>
2023-01-02 19:50:23 +0100

    Ranges.

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,14 @@ impl Range {
         self.offset / CHUNK_SIZE
     }
 
+    pub fn num_chunks(&self) -> u64 {
+        if self.is_chunk() {
+            1
+        } else {
+            (self.length + CHUNK_SIZE - 1) / CHUNK_SIZE
+        }
+    }
+
     pub fn is_chunk(&self) -> bool {
         self.length <= CHUNK_SIZE
     }
@@ -64,7 +72,7 @@ impl Range {
     }
 
     pub fn intersects(&self, other: &Range) -> bool {
-        !(self.end() < other.offset() || self.offset() > other.end())
+        !(self.end() <= other.offset() || self.offset() >= other.end())
     }
 }
 
@@ -75,14 +83,43 @@ impl std::fmt::Display for Range {
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
+enum Children {
+    None,
+    Data,
+    Tree(Box<(Node, Node)>),
+}
+
+impl Children {
+    fn is_none(&self) -> bool {
+        self == &Self::None
+    }
+
+    fn as_deref(&self) -> Option<&(Node, Node)> {
+        match self {
+            Self::Tree(nodes) => Some(&**nodes),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Node {
     range: Range,
     is_root: bool,
     hash: Hash,
-    children: Option<(Box<Node>, Box<Node>)>,
+    children: Children,
 }
 
 impl Node {
+    pub fn root(hash: Hash, length: u64) -> Self {
+        Self {
+            range: Range::new(length),
+            is_root: true,
+            hash,
+            children: Children::None,
+        }
+    }
+
     pub fn new(bytes: &[u8]) -> Self {
         let range = Range::new(bytes.len() as _);
         Self::inner_new(range, true, bytes)
@@ -93,14 +130,14 @@ impl Node {
         if let Some((left_range, right_range)) = range.split() {
             let at = left_range.end() - left_range.offset();
             let (left_bytes, right_bytes) = bytes.split_at(at as _);
-            let left = Box::new(Node::inner_new(left_range, false, left_bytes));
-            let right = Box::new(Node::inner_new(right_range, false, right_bytes));
+            let left = Node::inner_new(left_range, false, left_bytes);
+            let right = Node::inner_new(right_range, false, right_bytes);
             let hash = blake3::guts::parent_cv(left.hash(), right.hash(), is_root);
             Self {
                 range,
                 is_root,
                 hash,
-                children: Some((left, right)),
+                children: Children::Tree(Box::new((left, right))),
             }
         } else {
             let hash = blake3::guts::ChunkState::new(range.index())
@@ -110,7 +147,7 @@ impl Node {
                 range,
                 is_root,
                 hash,
-                children: None,
+                children: Children::Data,
             }
         }
     }
@@ -132,25 +169,38 @@ impl Node {
     }
 
     pub fn left(&self) -> Option<&Node> {
-        self.children.as_ref().map(|(left, _)| &**left)
+        self.children.as_deref().map(|(left, _)| left)
     }
 
     pub fn right(&self) -> Option<&Node> {
-        self.children.as_ref().map(|(_, right)| &**right)
+        self.children.as_deref().map(|(_, right)| right)
     }
 
     pub fn complete(&self) -> bool {
         if self.is_chunk() {
-            true
-        } else if let Some((left, right)) = self.children.as_ref() {
+            !self.children.is_none()
+        } else if let Some((left, right)) = self.children.as_deref() {
             left.complete() && right.complete()
         } else {
             false
         }
     }
 
-    pub fn length(&self) -> u64 {
-        self.range.length()
+    pub fn last_chunk(&self) -> &Node {
+        if let Some(right) = self.right() {
+            right.last_chunk()
+        } else {
+            self
+        }
+    }
+
+    pub fn length(&self) -> Option<u64> {
+        let last = self.last_chunk();
+        if last.children == Children::Data {
+            Some(last.range().end())
+        } else {
+            None
+        }
     }
 
     pub fn extract(&self, range: &Range) -> Option<Self> {
@@ -158,22 +208,56 @@ impl Node {
             range: self.range,
             is_root: self.is_root,
             hash: self.hash,
-            children: None,
+            children: Children::None,
         };
+        let intersects = self.range.intersects(range);
         if self.is_chunk() {
+            if intersects {
+                node.children = Children::Data;
+            }
             return Some(node);
         }
-        let (left, right) = self.children.as_ref()?;
-        if self.range.intersects(range) {
+        let (left, right) = self.children.as_deref()?;
+        if intersects {
             let left = left.extract(range)?;
             let right = right.extract(range)?;
-            node.children = Some((Box::new(left), Box::new(right)));
+            node.children = Children::Tree(Box::new((left, right)));
         }
         Some(node)
     }
 
-    pub fn verify(&mut self, _root: &Hash, _other: &Self) -> Result<()> {
+    pub fn verify(&mut self, _tree: &Self, _bytes: &[u8]) -> Result<()> {
         todo!()
+    }
+
+    fn inner_ranges(&self, ranges: &mut Vec<Range>) {
+        if self.complete() {
+            ranges.push(self.range);
+        } else if let Some((left, right)) = self.children.as_deref() {
+            left.inner_ranges(ranges);
+            right.inner_ranges(ranges);
+        }
+    }
+
+    pub fn ranges(&self) -> Vec<Range> {
+        let mut ranges = Vec::with_capacity(self.range.num_chunks() as _);
+        self.inner_ranges(&mut ranges);
+        ranges
+    }
+
+    fn inner_missing_ranges(&self, ranges: &mut Vec<Range>) {
+        if let Some((left, right)) = self.children.as_deref() {
+            left.inner_missing_ranges(ranges);
+            right.inner_missing_ranges(ranges);
+        } else if self.children == Children::None {
+            ranges.push(self.range);
+        }
+    }
+
+    pub fn missing_ranges(&self) -> Vec<Range> {
+        let mut ranges = Vec::with_capacity(self.range.num_chunks() as _);
+        self.inner_missing_ranges(&mut ranges);
+        ranges
     }
 
     pub fn encode(&self) -> Vec<u8> {
@@ -181,14 +265,6 @@ impl Node {
     }
 
     pub fn decode(_bytes: &[u8]) -> Result<Self> {
-        todo!()
-    }
-
-    pub fn ranges(&self) -> Vec<Range> {
-        todo!()
-    }
-
-    pub fn missing_ranges(&self, _length: u64) -> Vec<Range> {
         todo!()
     }
 }
@@ -230,10 +306,39 @@ mod tests {
             let input = &buf[..(case as _)];
             let expected = blake3::hash(input);
             let tree = Node::new(input);
-            dbg!(&tree);
+            //dbg!(&tree);
             assert_eq!(tree.hash(), &expected);
             assert!(tree.complete());
-            assert_eq!(tree.length(), case);
+            assert_eq!(tree.length(), Some(case));
+            assert_eq!(tree.ranges(), vec![tree.range]);
+            assert_eq!(tree.missing_ranges(), vec![]);
+
+            if let Some((left, right)) = tree.range().split() {
+                dbg!(left);
+                let left_tree = tree.extract(&left).unwrap();
+                dbg!(&left_tree);
+                assert_eq!(left_tree.hash(), &expected);
+                assert!(!left_tree.complete());
+                assert_eq!(left_tree.length(), None);
+                assert_eq!(left_tree.ranges(), vec![left]);
+                assert_eq!(left_tree.missing_ranges(), vec![right]);
+
+                dbg!(right);
+                let right_tree = tree.extract(&right).unwrap();
+                dbg!(&right_tree);
+                assert_eq!(right_tree.hash(), &expected);
+                assert!(!right_tree.complete());
+                assert_eq!(right_tree.length(), Some(case));
+                assert_eq!(right_tree.ranges(), vec![right]);
+                assert_eq!(right_tree.missing_ranges(), vec![left]);
+
+                let mut tree2 = Node::root(*tree.hash(), tree.range().length());
+                let (left_bytes, right_bytes) = buf.split_at(left.end() as _);
+
+                //tree2.verify(&left_tree, left_bytes).unwrap();
+                //tree2.verify(&right_tree, right_bytes).unwrap();
+                //assert_eq!(tree, tree2);
+            }
         }
     }
 }
