7591f4830c76f6680c303add7b13d7cf240b573d
David Craven <david@craven.ch>
2023-10-07 16:41:07 +0200

    Remove packager & static.

--- build_docker.sh
+++ build_docker.sh
@@ -3,5 +3,4 @@ cargo build -p peershare --target x86_64-unknown-linux-musl --release
 rm -r target/docker/
 mkdir -p target/docker/
 mv target/x86_64-unknown-linux-musl/release/peershare target/docker/
-cp -r static target/docker/
 docker build target/docker/ -f Dockerfile -t peershare/peershare
